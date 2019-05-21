bf0b24429428cfd979823945052510913dcc03ea
Henriette <Henr8588@edu.eal.dk>
2019-05-22 00:07:14 +0200

    tilføjet js til pb side

--- Sportspark/js/script.js
+++ Sportspark/js/script.js
@@ -1,7 +1,8 @@
 
 //Javascript til dropdown
 function openDropdown() {
-  var x = document.getElementById("drop");
+
+var x = document.getElementById("drop");
     
   if (x.style.display === "block") {
     x.style.display = "none";
@@ -32,8 +33,38 @@ function openOversigt() {
 }
 
 
+//Javascript til prisoversigt 
 
-function selected(x) {
-  x.classList.toggle("fas fa-check");
-    
+//event listener. 
+var classname = document.getElementsByClassName("itm");
+for (var i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('click', show);
 }
+
+//variabler - arrays 
+let antal = [];
+antal[0] = "itm1"; 
+antal[1] = "itm2"; 
+
+let tekst = [];
+tekst[0] = "børn samlet";
+tekst[1] = "voksne samlet";
+
+let output =[];
+output[0] = "txt1";
+output[1] = "txt2";
+
+//funktion son får teksten til at vise sig
+function show(){
+    for( var x = 0; x < classname.length ; x++) {
+        if(document.getElementById(antal[x]).value > 0) {
+            var a = document.getElementById(antal[x]).value + " " + tekst[x];
+        }
+        
+        else{ 
+            var a = "";
+        }
+    document.getElementById(output[x]).innerHTML = a;
+    }    
+}
+
