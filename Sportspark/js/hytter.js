dbf9cbdaab9f1661a4352ccab2cdb87f3d0231d7
Neli0013 <47401949+Neli0013@users.noreply.github.com>
2019-05-22 15:42:38 +0200

    Hytter
    Der belv kodet hen og tilbage med hytternes galleri arbjde og prøvede at få det til og virke, men uden held. Det vil nu blive opdateret, således at den er klar til og blive kigget på til i morgen.

---
+++ Sportspark/js/hytter.js
@@ -0,0 +1,30 @@
+let sliderlist = []; 
+sliderlist[0] = "../images/basishytterne/basis1.jpg";
+sliderlist[1] = "../images/basishytterne/basis2.jpg";
+sliderlist[2] = "../images/basishytterne/basis3.jpg";
+sliderlist[3] = "../images/basishytterne/basis4.jpg";
+
+let counter = 0;
+document.getElementById("sliderInfo").innerText = (counter + 1) + " af " + sliderlist.length;
+
+document.getElementsById ("bntNextWhite").addEventListener("click", function() {
+    counter++;
+
+if (counter >= sliderlist.length) {
+    counter = 0;
+}
+
+    document.getElementsByClassName("hytimg").src = sliderlist [counter];
+    document.getElementById("sliderInfo").innerText = (counter + 1) + " af " + sliderlist.length;
+})
+
+document.getElementsById("btnPrevWhite").addEventListener("click", function(){
+    counter--;
+    
+    if (counter <0){
+        counter = sliderlist.length - 1;
+    }
+    
+    document.getElementsByClassName("hytimg").src = sliderlist [counter];
+    document.getElementById("sliderInfo").innerText = (counter + 1) + " af " + sliderlist.length;
+})
