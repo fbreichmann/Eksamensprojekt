e4bc27e0db220d65bb3a98e98c4a91144aef02b7
Henriette <Henr8588@edu.eal.dk>
2019-05-23 08:20:14 +0200

    Ændret var til let i js kode

--- Sportspark/js/script.js
+++ Sportspark/js/script.js
@@ -2,7 +2,7 @@
 //Javascript til dropdown
 function openDropdown(i) {
 
-var x = document.getElementById(i);
+let x = document.getElementById(i);
     
   if (x.style.display === "block") {
     x.style.display = "none";
@@ -11,12 +11,11 @@ var x = document.getElementById(i);
   }
 }
 
-
 //Javascript til prisoversigt 
 
 //event listener. 
-var classname = document.getElementsByClassName("input");
-for (var i = 0; i < classname.length; i++) {
+let classname = document.getElementsByClassName("input");
+for (let i = 0; i < classname.length; i++) {
     classname[i].addEventListener('change', show);
 }
 //variabler - arrays 
@@ -49,7 +48,7 @@ output[6] = "output7";
 
 //funktion son får teksten til at vise sig
 function show(){
-    for( var x = 0; x < classname.length ; x++) {
+    for( let x = 0; x < classname.length ; x++) {
         if(document.getElementById(antal[x]).value > 0) {
     var a = document.getElementById(antal[x]).value + " " + tekst[x];
     }
@@ -64,15 +63,15 @@ function show(){
 
 //Javascript til billedskifte og baggrunds farve skift
 
-var knap = document.getElementsByClassName("knap");
-var imgskift = new Array(knap.length);
+let knap = document.getElementsByClassName("knap");
+let imgskift = new Array(knap.length);
 
-for ( var i = 0; i<knap.length;i++) {
+for ( let i = 0; i<knap.length;i++) {
 imgskift[i]='plus';     
 }
 
 function changeImg(a,b,c){
-    var image = document.getElementById(a);
+    let image = document.getElementById(a);
     if (imgskift[b]=='plus'){
         image.src='../images/check.svg';
         document.getElementById(c).style.opacity=0.3;  
@@ -84,4 +83,4 @@ function changeImg(a,b,c){
         imgskift[b]='plus'; 
     }
 }
-   
+
